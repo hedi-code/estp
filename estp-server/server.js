bd396c22f7c569e52e559552a089143ed04b380a
BEN SAFEGINE Mohamed Hedi <mohamedhedi.bensafegine@amaris.com>
2025-04-27 11:33:01 +0100

    Fix: add test config

--- estp-server/server.js
+++ estp-server/server.js
@@ -12,10 +12,16 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:4200',  // Allow requests from this origin
-    credentials: true,  // Allow credentials (cookies) to be included in the request
-  };
-  
+  origin: ['*', 'http://localhost:4200', 'https://test.new.app.forumestp.fr', 'https://www.test.new.app.forumestp.fr', 'https://www.new.app.forumestp.fr', 'https://new.app.forumestp.fr'  // Allow requests from this origin
+  ], 
+  credentials: true,  // Allow credentials (cookies) to be included in the request
+};
+  const env = process.env.NODE_ENV || 'development';
+
+// Dynamically load corresponding .env file
+dotenv.config({ path: `.env.${env}` });
+
+console.log(`Loaded environment: ${env}`);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
