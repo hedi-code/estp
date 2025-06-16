e6ea80fefff6490f5598c65a7d0bd2d5de774714
hedi <hedibensafegine7@gmail.com>
2025-06-16 08:06:48 +0100

    add navbar.

--- estp-client/src/app/features/forum/layout/layout.component.ts
+++ estp-client/src/app/features/forum/layout/layout.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LayoutService } from '../services/layout.service';
+import { AuthCookieService } from '../../../core/services/auth-cookie.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -8,7 +10,14 @@ import { LayoutService } from '../services/layout.service';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  nom: string =''
 
-  constructor(public layoutService:LayoutService){}
-
+  constructor(public layoutService:LayoutService, private authCookieService: AuthCookieService, private router:Router){}
+ngOnInit(){
+      this.nom = this.authCookieService.getFirstName() + " " + this.authCookieService.getLastName();
+}
+  logout(){
+    this.authCookieService.logout()
+    this.router.navigateByUrl('');
+  }
 }
