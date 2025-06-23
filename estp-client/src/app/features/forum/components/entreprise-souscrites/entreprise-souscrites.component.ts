bc0e344f338098249350425b7a6933c157a7ab89
hedi <hedibensafegine7@gmail.com>
2025-06-23 14:50:47 +0100

    Ajout entreprise works.

--- estp-client/src/app/features/forum/components/entreprise-souscrites/entreprise-souscrites.component.ts
+++ estp-client/src/app/features/forum/components/entreprise-souscrites/entreprise-souscrites.component.ts
@@ -6,6 +6,10 @@ import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
 import { Commande1 } from '../../models/commande1.model';
 import { MessageService } from 'primeng/api';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../auth/auth.service';
+import { ContactService } from '../../models/contact.service';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-entreprise-souscrites',
@@ -20,19 +24,39 @@ export class EntrepriseSouscritesComponent implements OnInit {
   cmdAssigner: Entreprise | undefined;
   assignModel = false;
   role: String | null = ''
-  
+  createDialog = false;
+  registerForm: FormGroup;
+  labelSiren = "Numéro SIREN de l'entreprise";
 
-  constructor(private entrepriseService: EntrepriseService, private userService: UserService, private cookieService: AuthCookieService, private messageService: MessageService) {
+
+  constructor(private fb: FormBuilder, private entrepriseService: EntrepriseService, 
+    private authService: AuthService,    private contactService: ContactService,
+    private userService: UserService, private cookieService: AuthCookieService, 
+    private messageService: MessageService) {
+    this.registerForm = this.fb.group({
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      phonenumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      companyName: ['', Validators.required],
+      companySiren: ['', [Validators.required, Validators.pattern('^(\\d\\s*){4,15}$')]], // Example pattern for SIREN number
+      isNotInFrance: [false],
+      functionInCompany: ['', Validators.required],
+      password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{10,}$/)]],
+      confirmPassword: ['', [Validators.required]],
+    }, {
+      validator: this.passwordMatcher('password', 'confirmPassword')
+    });
 
   }
   ngOnInit(): void {
     this.role = this.cookieService.getRole();
     this.entrepriseService.getAllEntreprises().subscribe({
       next: (response) => {
-        if(this.cookieService.getRole() == "comm"){
+        if (this.cookieService.getRole() == "comm") {
           this.entreprises = response.filter(e => e.commercial_id == Number(this.cookieService.getUserId()));
         }
-        else{
+        else {
           this.entreprises = response;
         }
       },
@@ -45,30 +69,107 @@ export class EntrepriseSouscritesComponent implements OnInit {
       error: (err) => console.log(err)
     })
   }
-
-  getCommercialData(id: number):User | undefined{
+  // Custom Validator to check if password and confirmPassword match
+  passwordMatcher(password: string, confirmPassword: string) {
+    return (group: FormGroup) => {
+      const passwordControl = group.get(password);
+      const confirmPasswordControl = group.get(confirmPassword);
+      if (confirmPasswordControl?.errors && !confirmPasswordControl?.errors['passwordMismatch']) {
+        return;
+      }
+      if (passwordControl?.value !== confirmPasswordControl?.value) {
+        confirmPasswordControl?.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl?.setErrors(null);
+      }
+    };
+  }
+  getCommercialData(id: number): User | undefined {
     return this.commercials.find(u => u.id == id);
   }
-  assigner(cmd: Entreprise){
+  assigner(cmd: Entreprise) {
     this.cmdAssigner = cmd;
     this.assignModel = true;
   }
-  onRowDoubleClick(event: any){
-if (this.cmdAssigner) {
-  this.cmdAssigner.commercial_id = event.id;
-  console.log(event)
-    console.log(this.cmdAssigner)
-  this.entrepriseService.updateEntreprise(this.cmdAssigner.id ?? 9999, this.cmdAssigner).subscribe({
-    next: (response) => {
-      this.messageService.add({ severity: 'success', summary: '', detail: 'Commercial assigné', life: 2000 });
-      this.assignModel = false;
-    },
-    error: (err) => {
-      this.messageService.add({ severity: 'error', summary: '', detail: 'Database error', life: 2000 });
+  onRowDoubleClick(event: any) {
+    if (this.cmdAssigner) {
+      this.cmdAssigner.commercial_id = event.id;
+      console.log(event)
+      console.log(this.cmdAssigner)
+      this.entrepriseService.updateEntreprise(this.cmdAssigner.id ?? 9999, this.cmdAssigner).subscribe({
+        next: (response) => {
+          this.messageService.add({ severity: 'success', summary: '', detail: 'Commercial assigné', life: 2000 });
+          this.assignModel = false;
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: '', detail: 'Database error', life: 2000 });
 
+        }
+      })
     }
-  })
-}
-    
+
   }
+  onCheckboxChange() {
+    const isNotInFrance = this.registerForm.get('isNotInFrance')?.value;
+    const companySirenControl = this.registerForm.get('companySiren');
+
+    if (isNotInFrance) {
+      // Remove the required validator if the checkbox is checked
+      companySirenControl?.clearValidators();
+      this.labelSiren = "Matricule fiscal"
+    } else {
+      // Re-apply the required validator if the checkbox is unchecked
+      companySirenControl?.setValidators([Validators.required,Validators.pattern('^[0-9]{9}$')]);
+      this.labelSiren = "Numéro SIREN de l'entreprise"
+    }
+
+    // Update the validity of the control
+    companySirenControl?.updateValueAndValidity();
+  }
+  openCreateDialog() {
+    this.createDialog = true;
+  }
+   onSubmit() {
+      if (this.registerForm.valid) {
+        const formData = this.registerForm.value;
+    
+        this.authService.register(formData.email, formData.password, formData.lastname, formData.firstname, true).pipe(
+          switchMap((user) => {
+            return this.contactService.createContact({
+              user_id: user.user.id,
+              nom: formData.lastname,
+              prenom: formData.firstname,
+              email: formData.email,
+              telephone1: formData.phonenumber,
+              fonction: formData.functionInCompany,
+              standiste: false,
+            }).pipe(
+              map((contact) => ({ user, contact }))
+            );
+          }),
+          switchMap(({ user, contact }) => {
+            return this.entrepriseService.createEntreprise({
+              secteur_id: 1,
+              nom: formData.companyName,
+              logo: '',
+              siren: formData.companySiren,
+              contact_principal_id: contact.id, // Assuming contact has an ID
+              user_id: user.user.id,
+              activity: 0
+            });
+          })
+        ).subscribe({
+          next: (entreprise) => {
+            console.log('Entreprise created:', entreprise);
+            // Handle success (e.g., navigate or show success message)
+          },
+          error: (error) => {
+            console.error('Error in registration flow:', error);
+          }
+        });
+    
+      } else {
+        console.log('Form is invalid');
+      }
+    }
 }
