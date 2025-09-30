d7b309708ab574d05adc3a94aea20ffb119eacd6
hedi <hedibensafegine7@gmail.com>
2025-09-30 19:34:46 +0100

    Feat: fix facture et ordre categorie.

--- estp-client/src/app/features/forum/components/bc2-souscrits/bc2-souscrit-facture/bc2-souscrit-facture.component.ts
+++ estp-client/src/app/features/forum/components/bc2-souscrits/bc2-souscrit-facture/bc2-souscrit-facture.component.ts
@@ -126,6 +126,32 @@ export class Bc2SouscritFactureComponent implements OnInit {
     return totalTVA;
   }
 
+  // Group options by TVA rate
+  getTVAGroups(): any[] {
+    const groups: { [key: number]: any } = {};
+
+    this.getFilteredOptions().forEach(option => {
+      const tauxTVA = option.option_taux_tva || 20;
+      const prixHT = (option.option_prix_ht || 0) * (option.qty || 1);
+      const montantTVA = prixHT * (tauxTVA / 100);
+
+      if (!groups[tauxTVA]) {
+        groups[tauxTVA] = {
+          taux: tauxTVA,
+          totalHT: 0,
+          totalTVA: 0,
+          totalTTC: 0
+        };
+      }
+
+      groups[tauxTVA].totalHT += prixHT;
+      groups[tauxTVA].totalTVA += montantTVA;
+      groups[tauxTVA].totalTTC += prixHT + montantTVA;
+    });
+
+    return Object.values(groups).sort((a, b) => a.taux - b.taux);
+  }
+
   getCompanyInfo() {
     if (!this.modifyCommandeFact?.entreprise) return null;
     const entreprise = this.modifyCommandeFact.entreprise;
@@ -149,5 +175,5 @@ export class Bc2SouscritFactureComponent implements OnInit {
 
 
 
- 
+
 }
